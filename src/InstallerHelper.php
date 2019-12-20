957404e5b5554689d24eb32e4ba35449bf7e73a9
Nemanja Avramović <avramyu@gmail.com>
2019-12-20 10:37:41 +0100

    first commit

---
+++ src/InstallerHelper.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Avram\ComposerCaffeinateedModuleInstaller;
+
+use Composer\Installers\BaseInstaller;
+
+class InstallerHelper extends BaseInstaller {
+
+	function getLocations() {
+		// it will be looking for a key of FALSE, which evaluates to 0, i.e. the first element
+		// that element value being false signals the installer to use the default path
+		return array( false );
+	}
+
+    protected function templatePath($path, array $vars = array())
+    {
+        if (strpos($path, '{') !== false) {
+            extract($vars);
+            preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
+            if (!empty($matches[1])) {
+                foreach ($matches[1] as $var) {
+                    $path = str_replace('{$' . $var . '}', ucfirst($$var), $path);
+                }
+            }
+        }
+
+        return $path;
+    }
+
+}
